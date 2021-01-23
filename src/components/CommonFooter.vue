01e1563172ed453ed0a67f61fadc20559981aa87
Arashi2000 <76526188+Arashi2000@users.noreply.github.com>
2021-01-23 10:03:08 +0900

    src
    srcをコミットしました。現在、firstviewとheaderが表示されません。

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,34 @@
+<template>
+   <footer class="top-footer">
+        <div class="footer-bar">
+          <div class="flex2">
+            <h1 class="footer-logo">estra inc.</h1>
+          </div>
+        </div>
+        <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+      </footer>
+</template>
+
+<style scoped>
+
+.footer-bar {
+  height: 70px;
+  line-height: 70px;
+}
+
+.top-footer {
+  background: #000;
+  text-align: center;
+}
+
+.top-footer small {
+  text-align: center;
+  color: #fff;
+}
+
+
+.footer-logo {
+  color: #fff;
+}
+
+</style>
